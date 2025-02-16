c7a73376f6bd0cecc05b73e623336b4eb5cce04b
tokotmg <92723132+tokotmg@users.noreply.github.com>
2025-02-16 14:56:08 +0400

    Add files via upload

---
+++ day 1/homework5.py
@@ -0,0 +1,9 @@
+width = float(input("Enter the width of the rectangle : "))
+length = float(input("Enter the length of the rectangle : "))
+
+area = width * length
+
+perimeter = (width + length) * 2
+
+print("Area of the rectangle is %2.f" %area)
+print("Perimeter of the rectangle is %2.f" %perimeter)
