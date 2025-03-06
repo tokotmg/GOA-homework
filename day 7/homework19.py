cc7a77fb05d1923ed27b7806076d3738dc6dbe55
tokotmg <92723132+tokotmg@users.noreply.github.com>
2025-03-06 10:19:57 +0400

    Add files via upload

---
+++ day 7/homework19.py
@@ -0,0 +1,11 @@
+a = int(input("Enter 1st number:"))
+b = int(input("Enter 2nd number:"))
+c = int(input("Enter 3rd number:"))
+d = int(input("Enter 4th number:"))
+e = int(input("Enter 5th number:"))
+add = a + b + c + d + e
+print("Addition is", add)
+average = add / 5
+print("Average =",average)
+cube = a + b + c + d + e ** 3
+print(f"Cube is: {cube}")
