d41ec3c0eddaabc027c5407cf86a9ebeee578c02
tokotmg <92723132+tokotmg@users.noreply.github.com>
2025-03-11 17:51:34 +0400

    Delete day 3 directory

--- day 3/homework7.py
+++
@@ -1,6 +0,0 @@
-name = "Tornike"
-company = "GOA"
-number = 190
-print(type(name))
-print(type(company))
-print(type(number))
--- day 3/homework9.py
+++
@@ -1,2 +0,0 @@
-print("Hello World")
-# print("Hello World") იქნება სწორი, იმიტომ რომ  ორი პჭყალი აქვს.
