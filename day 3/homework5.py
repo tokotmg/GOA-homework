c5d663c0b2f400e624eb4c3026172b8d9522026a
tokotmg <92723132+tokotmg@users.noreply.github.com>
2025-03-13 14:58:34 +0400

    Delete day 3 directory

--- day 3/homework5.py
+++
@@ -1,9 +0,0 @@
-width = float(input("Enter the width of the rectangle : "))
-length = float(input("Enter the length of the rectangle : "))
-
-area = width * length
-
-perimeter = (width + length) * 2
-
-print("Area of the rectangle is %2.f" %area)
-print("Perimeter of the rectangle is %2.f" %perimeter)
