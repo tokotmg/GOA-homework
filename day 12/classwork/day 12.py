5d172561a933e302be4a324437004b339239542e
tokotmg <92723132+tokotmg@users.noreply.github.com>
2025-03-19 15:43:35 +0400

    Add files via upload

---
+++ day 12/classwork/day 12.py
@@ -0,0 +1,6 @@
+number = input("Enter a number: ")
+print(type(number))
+number=int(number)
+print(type(number))
+number=float(number)
+print(type(number))
---
+++ day 12/homework/homework25.py
@@ -0,0 +1,4 @@
+string = "100"
+integer = 100
+num = int(string)
+print(num + integer)
