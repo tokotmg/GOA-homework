31e7d3faf76e4d60d3e31efe655cab79c2099b7d
tokotmg <92723132+tokotmg@users.noreply.github.com>
2025-03-25 20:24:29 +0400

    Delete day 6 directory

--- day 6/classwork/day 6.py
+++
@@ -1,8 +0,0 @@
-# name = "vano"
-# age = 0
-# print(name + "best")
-# კომენტარი ნიშნავს ტექსტი, რომელიც არ ჩანს როცა დააპრინტე.
-name = "vano"
-surname = "motiashvili"
-height = "1.86"
-print(name + surname + height)
