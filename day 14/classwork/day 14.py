85549c1282c584ffb0ce6157b31dad6fd1d9944d
tokotmg <92723132+tokotmg@users.noreply.github.com>
2025-03-28 08:12:36 +0400

    Add files via upload

---
+++ day 14/classwork/day 14.py
@@ -0,0 +1,11 @@
+print (3 == 3) #True
+print (6 >= 7) #False
+print (5 < 6) #True
+my_name = "Tornike"
+
+user_name = input("Enter your name:")
+
+print(my_name == user_name)
+
+
+
---
+++ day 14/homework/homework28.py
@@ -0,0 +1,5 @@
+email = "example@email.com"
+password = "9000" 
+user_email = input("Enter your email: ")
+user_password = input("Enter a Password: ")
+print(email == user_email, password == user_password)
