aa0e36d603dd8342d20f672e76e8be65bbea1a5d
tokotmg <92723132+tokotmg@users.noreply.github.com>
2025-04-05 08:16:42 +0400

    Add files via upload

---
+++ day 16/classwork/day 16.py
@@ -0,0 +1,3 @@
+print(True and False and True or False and True and True or False or False and True)#false
+print(False or True and True or False and True or True and False)#true
+print(10 > 5 and True or False and 23 == 23 and False or 100 / 100 == 1 or 5 * 5 == 30 and 10 < 30 > 20)#true
