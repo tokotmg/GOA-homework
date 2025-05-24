8a4cb30cd5dd9495e670259b6c85283b277a1ce4
tokotmg <92723132+tokotmg@users.noreply.github.com>
2025-05-24 08:51:39 +0400

    Add files via upload

---
+++ day 30/classwork/day 30 (1).py
@@ -0,0 +1,4 @@
+cars = ["Toyota", "Porsche", "Ferrari", "Tesla", "Jeep"]
+print(cars)
+numbers = [0, 2, 4, 6, 8, 10]
+print(numbers)
---
+++ day 30/classwork/day 30 (2).py
@@ -0,0 +1,6 @@
+name = ["Tornike", "Nika", "Vano", "Giorgi", "Vako"]
+print(name[3])
+name[3]="Tornike"
+print(name)
+numbers = [5, 10, 15]
+print(numbers[0] + numbers[2])
