d47ab96407da3086636d1e836fbb6dd9e9bda996
tokotmg <92723132+tokotmg@users.noreply.github.com>
2026-01-18 08:49:39 +0400

    Add files via upload

---
+++ day 97/classwork/index.js
@@ -0,0 +1,22 @@
+// var საკვანძო სიტყვა გამოიყენება ცვლადის გამოსაცხადებლად
+// let საკვანძო სიტყვა გამოიყენება ცვლადების დეკლარირებისთვის, რომლებიც ბლოკ-სკოპიანია და რომლებსაც მოგვიანებით შეიძლება მნიშვნელობის მინიჭება.
+// const საკვანძო სიტყვა გამოიყენება იმ ცვლადების გამოსაცხადებლად, რომელთა ხელახლა მინიჭება შეუძლებელია მათი საწყისი მნიშვნელობის დაყენების შემდეგ.
+var favoriteFood = "Pizza";
+console.log("Original var:", favoriteFood);
+favoriteFood = "Tacos";
+console.log("Changed var:", favoriteFood);
+let temperature = 72;
+console.log("Original let:", temperature);
+temperature = 75;
+console.log("Changed let:", temperature);
+const pi = 3.14159;
+console.log("Original const:", pi);
+let customerOrderDetails = { id: 123, item: "Laptop" };
+const maximumAttempts = 3;
+console.log
+//typeof ოპერატორი აბრუნებს JavaScript ცვლადის მონაცემთა ტიპს.
+console.log(typeof "Nika")
+console.log(typeof 123)
+console.log(typeof true)
+// ეს შეცდომაა, რადგან const საკვანძო სიტყვის ხელახლა მინიჭება შეუძლებელია.
+
