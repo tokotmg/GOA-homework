5f312c1bcf0d2c7d1d0eec06ca6d65b19e2f7c58
tokotmg <92723132+tokotmg@users.noreply.github.com>
2026-02-22 09:30:38 +0400

    Add files via upload

---
+++ day 107/classwork/index.js
@@ -0,0 +1,8 @@
+const list = ["milk", "bread", "cheese", "meat", "potato"]
+list.push("butter", "tomato", "lettuce")
+list.unshift("mayonnaise", "ham")
+list.pop()
+list.pop()
+list.shift()
+console.log(list)
+console.log(list.length)
